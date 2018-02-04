537ca6e09ec0b7a094fd39e37945bbc3918c2f03
canisterism <33423125+canisterism@users.noreply.github.com>
2018-02-04 23:20:01 +0900

    Add files via upload

---
+++ slackbot_settings.py
@@ -0,0 +1,8 @@
+#botアカウントのトークン設定
+API_TOKEN = "xoxb-308189861139-8vAA3KnvrQBzTt8pF5hMJaFb"
+
+#このbot宛てのメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "え？よく聞こえない"
+
+#プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
